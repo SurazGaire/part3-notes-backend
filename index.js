60444fd461c6337808945e736dd6c993983509ae
SurazGaire <luxheartsuraz@gmail.com>
2022-05-03 15:21:11 +0545

    refactor : index js file

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-<<<<<<< Updated upstream
-// require("dotenv").config();
-const express = require("express");
-const Note = require("./models/note");
-=======
 const app = require("./app"); // the actual Express application
 const http = require("http");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
->>>>>>> Stashed changes
 
 const server = http.createServer(app);
 
