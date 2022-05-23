ac0714c08dd1c86b834b656a40f6b84a77902426
SurazGaire <luxheartsuraz@gmail.com>
2022-05-23 13:26:54 +0545

    feat : Auth users

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,4 +1,6 @@
 const Note = require("../models/note");
+const Note = require("../models/user");
+
 const initialNotes = [
   {
     content: "HTML is easy",
@@ -24,8 +26,14 @@ const notesInDb = async () => {
   return notes.map((note) => note.toJSON());
 };
 
+const usersInDb = async () => {
+  const users = await User.find({});
+  return users.map((u) => u.toJSON());
+};
+
 module.exports = {
   initialNotes,
   nonExistingId,
   notesInDb,
+  usersInDb,
 };
